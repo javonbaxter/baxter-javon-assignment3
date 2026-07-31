3cff702ee93dfb669ca804b8ce2f5cb0cdc7c260
javonbaxter <baxterjavon999@gmail.com>
2026-07-30 21:50:05 -0500

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,32 @@
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-01
+
+      Project to validate a form used for setting up a new account
+      Author:
+      Date:
+
+      Filename: task1.js
+*/
+
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function () {
+
+    // Clear previous validation messages
+    pwd.setCustomValidity("");
+    pwd2.setCustomValidity("");
+
+    // Check password pattern
+    if (!pwd.checkValidity()) {
+        pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+    }
+
+    // Check that both passwords match
+    else if (pwd.value !== pwd2.value) {
+        pwd2.setCustomValidity("Your passwords must match");
+    }
+
+});
